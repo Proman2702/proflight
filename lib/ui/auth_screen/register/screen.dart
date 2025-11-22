7de4e62fa72c90b9941de98d6cac5da3d2d20aeb
Proman2702 <proman2702@gmail.com>
2025-11-22 13:18:39 +0300

    zhir

--- lib/ui/auth_screen/register/screen.dart
+++ lib/ui/auth_screen/register/screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:proflight/ui/auth_screen/register/view_model.dart';
+import 'package:provider/provider.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final model = context.watch<RegisterScreenModel>();
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -16,6 +20,8 @@ class RegisterScreen extends StatelessWidget {
 
               child: TextField(
                 decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Почта"),
+                controller: TextEditingController(text: model.email),
+                onChanged: (value) => model.setEmail(value),
               ),
             ),
             SizedBox(height: 5),
@@ -25,10 +31,12 @@ class RegisterScreen extends StatelessWidget {
 
               child: TextField(
                 decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Пароль"),
+                controller: TextEditingController(text: model.password),
+                onChanged: (value) => model.setPassword(value),
               ),
             ),
             SizedBox(height: 5),
-            ElevatedButton(onPressed: () {}, child: Text("Зарегаться")),
+            ElevatedButton(onPressed: () => model.leaveWithRegister(context), child: Text("Зарегаться")),
           ],
         ),
       ),
--- lib/ui/auth_screen/register/view_model.dart
+++ lib/ui/auth_screen/register/view_model.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:proflight/service/auth/auth_service.dart';
 
 class RegisterScreenModel extends ChangeNotifier {
-  String? _email;
-  String? _password;
+  String _email = "";
+  String _password = "";
 
   final AuthService _authService = AuthService();
 
-  void registerUser() async {
-    await _authService.signIn(_email!, _password!);
+  String get email => _email;
+  String get password => _password;
+
+  void setEmail(String value) {
+    _email = value;
+    notifyListeners();
+  }
+
+  void setPassword(String value) {
+    _password = value;
+    notifyListeners();
+  }
+
+  void _registerUser() async {
+    await _authService.signIn(_email, _password);
+  }
+
+  void leaveWithRegister(BuildContext context) {
+    _registerUser();
+    Navigator.pop(context);
   }
 }
